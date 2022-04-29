3b59d4691533c79e0fe1ef06bfd8b0456ede3e18
unknown <mateus.oliver_@outlook.com.br>
2022-04-29 14:59:30 -0300

    removendo codigos antigos

--- app/src/main/java/br/oliver/mark4/view/fragments/NotaFragment.kt
+++ app/src/main/java/br/oliver/mark4/view/fragments/NotaFragment.kt
@@ -6,51 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.oliver.mark4.R
+import br.oliver.mark4.databinding.BottomSheetAddTabBinding
+import br.oliver.mark4.databinding.FragmentNoteBinding
 
 class NotaFragment : Fragment() {
 
+    private lateinit var nomeTable: String
+
+    private lateinit var binding: FragmentNoteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         if (arguments != null) {
-            //nomeTable = arguments!!.getString(ARG_PARAM1)
+            nomeTable = requireArguments().getString(BDFragmentOpcoes.TAG).toString()
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentNoteBinding.inflate(inflater, container, false)
 
-        // Inflate the layout for this fragment
-        //recyclerTarefas = view!!.findViewById(R.id.recyclerTarefas)
-        //iv_iconVazio = view!!.findViewById(R.id.iv_iconVazio)
-        //listar()
-        return inflater.inflate(R.layout.fragment_note, container, false)
+        return binding.root
     }
 
-    override fun onResume() {
-        listar()
-        super.onResume()
-    }
-
-    fun listar() {
-        //notaDAO = NotaDAO(requireActivity())
-        //listTasks = notaDAO.listar(nomeTable)
-        //notaAdapter = NotaAdapter(listTasks)
-        //val layoutManager = StaggeredGridLayoutManager(
-        //    Grid_Largura.calculateNoOfColumns(requireActivity(), 180),
-        //    StaggeredGridLayoutManager.VERTICAL
-        //)
-        //recyclerTarefas.setLayoutManager(layoutManager)
-        //recyclerTarefas.setHasFixedSize(true)
-        //notaAdapter.setHasStableIds(true)
-        //Collections.reverse(listTasks)
-        //recyclerTarefas.setAdapter(notaAdapter)
-        //if (listTasks!!.isEmpty()) {
-        //    iv_iconVazio!!.visibility = View.VISIBLE
-        //    Glide.with(iv_iconVazio!!.context).load(R.drawable.ic_text_branco_gelo)
-        //        .into(iv_iconVazio)
-        //} else {
-        //    iv_iconVazio!!.visibility = View.GONE
-        //}
-    }
 
     companion object {
         private const val ARG = "table"
